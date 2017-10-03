b694529730aabd0df8222e6a1ac6953101ccf0f3
seanhann <hanjiaxinh@gmail.com>
2017-10-03 17:55:17 +0000

    support https

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+/*
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/', function(req, res, next) {
+    res.sendFile(path.join(__dirname + '/index.htm'));
+});
+*/
+router.get('/.well-known/acme-challenge/VezBVngpd_PRo3OIuwm9GzYBE10IDe6bI-n0cAecivk', function(req, res, next) {
+    res.set('Content-Type', 'text/plain');
+    res.send('teset');
+    //res.send(path.join(__dirname + '/.well-known/acme-challenge/VezBVngpd_PRo3OIuwm9GzYBE10IDe6bI-n0cAecivk'));
+    //res.send();
+});
 module.exports = router;
